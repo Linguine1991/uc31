68da25c5e831ec5d609364fb4a4f5f7de120b277
Linguine1991 <tgatga121261@gmail.com>
2026-06-03 08:32:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ def cadastro():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    @app.route('/')
+def formulario():
+    return render_template('index.html')
+
+@app.route('/validacao', methods=['POST'])
+def cadastro():
+    
+    nome = request.form.get('nome', '').strip().title()
+    email = request.form.get('email', '').strip().lower()
+    cidade = request.form.get('cidade', '').strip().title()
+    
+    return f"""
+    Nome: {nome}<br>
+    Email: {email}<br>
+    Cidade: {cidade}
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
